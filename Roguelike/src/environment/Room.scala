74a760cc86be9c17092a0359d900830a3149979b
Lucas <mestrecabecao@hotmail.com>
2012-12-06 14:18:07 -0200

    Funções para desenhar sala

--- Roguelike/src/environment/Room.scala
+++ Roguelike/src/environment/Room.scala
@@ -9,7 +9,50 @@ class Room(top : Int, left : Int, height : Int, width : Int) {
   var monsters : Map[(Int, Int), Monster] = populate(height, width)
   
   def draw(canvas : Array[Array[Char]]) = {
+    drawWalls(canvas)
+    drawFloor(canvas)
+    drawDoors(canvas)
+    drawPlayerCharacter(canvas)
+    drawMonsters(canvas)
+  }
+  
+  def drawWalls(canvas : Array[Array[Char]]) = {
+    canvas(top - 1)(left - 1) = '+'
+    canvas(top - 1)(left + width) = '+'
+    canvas(top + height)(left - 1) = '+'
+    canvas(top + height)(left + width) = '+'
     
+    for(i <- top until (top + height)) {
+      canvas[i][left - 1] = '|'
+      canvas[i][left + width] = '|'
+    }
+    
+    for(j <- left until (left + width)) {
+      canvas[top - 1][j] = '-'
+      canvas[top + height][j] = '-'
+    }
+  }
+  
+  def drawFloor(canvas : Array[Array[Char]]) = {
+    for(i <- 0 until height; j <- 0 until width) {
+      canvas[i + top][j + left] = map[i][j].toChar
+    }
+  }
+  
+  def drawDoors(canvas : Array[Array[Char]]) = {
+    
+  }
+  
+  def drawPlayerCharacter(canvas : Array[Array[Char]]) = {
+    if(playerCharacter.isSome) {
+      canvas(playerCharacterPosition._1)(playerCharacterPosition._2) = playerCharacter.toChar
+    }
+  }
+  
+  def drawMonsters(canvas : Array[Array[Char]]) = {
+    for(((i, j), monster) <- monsters) {
+      canvas(i)(j) = monster.toChar
+    }
   }
   
   def populate(height : Int, width : Int) : Map[(Int, Int), Monster] = {
